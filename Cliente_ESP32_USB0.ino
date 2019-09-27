c05685e54fb7d4089640378577006985f0932e93
matricce <ruviansanches20@gmail.com>
2019-09-27 00:44:02 -0300

    Add files via upload
    comunicação tcp ok

--- Cliente_ESP32_USB0.ino
+++ Cliente_ESP32_USB0.ino
@@ -1,11 +1,34 @@
 /*
- * Cliente
- * ESP32
- * /dev/ttyUSB0
- * /COM4
- */
- 
+   Cliente
+   ESP32
+   /dev/ttyUSB0
+   /COM4
+*/
+
+/*
+   CHECK é o botão que simula o gatilho para checar se a catraca pode ou não ser liberada
+   TICKET_GATE simula a catraca sendo girada para dentro (uma pessoa entrou)
+   EXIT simula a catraca sendo girada para fora (uma pessoa saiu)
+*/
 #include <WiFi.h>
+#define DEBOUNCE_INTERVAL 200 /*intervalo em milissegundos em que o botão pode ser pressionado novamente*/
+struct TicketGate {
+  const uint8_t PIN_BTN_CHECK;
+  const uint8_t PIN_BTN_TICKET_GATE;
+  const uint8_t PIN_EXIT;
+  const uint8_t PIN_LED_GREEN;
+  const uint8_t PIN_LED_RED;
+  bool checkStatus;
+  bool ticketGateStatus;
+  bool exitStatus;
+};
+
+TicketGate tGate[] = {
+  {15, 22, 35, 4, 16},
+  {13, 21, 34, 14, 27},
+  {26, 25, 39, 33, 32},
+  {17, 5, 36, 18, 19}
+};
 
 const char* ssid     = "NodeMCU_AP";
 const char* password = "pa$$word";
@@ -13,7 +36,82 @@ const char* password = "pa$$word";
 const char* host = "192.168.4.1";
 const uint16_t port = 555;
 
-void wifiSetup(){
+void pinsSetup();
+void wifiSetup();
+bool sendPayload(String message);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Iniciando...");
+  pinsSetup();
+  wifiSetup();//Estabelece conexão com o AP
+  sendPayload("Comunicação iniciada");//Envia a primeira mensagem para o servidor
+}
+
+void loop() {
+  bool buttonState;
+  static unsigned long delayButtonCheck[4] = {0, 0, 0, 0};
+  static unsigned long delayButtonTGate[4] = {0, 0, 0, 0};
+  static unsigned long delayButtonExit[4] = {0, 0, 0, 0};
+  for (int i = 0; i < 4; i++) {
+    if ((millis() - delayButtonCheck[i]) > DEBOUNCE_INTERVAL) {
+      buttonState = !digitalRead(tGate[i].PIN_BTN_CHECK);/*se o botão estiver pressionado, retorna LOW*/
+      if (buttonState) {
+        delayButtonCheck[i] = millis();
+        if (buttonState != tGate[i].checkStatus) { //Se o botão de CHECK for pressionado, executar esse bloco
+//          Serial.print(F("botão CHECK-"));
+//          Serial.print(i);
+//          Serial.println(F(" pressionado"));
+          sendPayload("botão CHECK-" + String(i) + " pressionado");
+        }
+        tGate[i].checkStatus = true;
+      }
+      tGate[i].checkStatus = buttonState;
+    }
+    if ((millis() - delayButtonTGate[i]) > DEBOUNCE_INTERVAL) {
+      buttonState = !digitalRead(tGate[i].PIN_BTN_TICKET_GATE);/*se o botão estiver pressionado, retorna LOW*/
+      if (buttonState) {
+        delayButtonTGate[i] = millis();
+        if (buttonState != tGate[i].ticketGateStatus) { //Se o botão de TICKET_GATE for pressionado, executar esse bloco
+//          Serial.print(F("botão TICKET_GATE-"));
+//          Serial.print(i);
+//          Serial.println(F(" pressionado"));
+          sendPayload("botão TICKET_GATE-" + String(i) + " pressionado");
+        }
+        tGate[i].ticketGateStatus = true;
+      }
+      tGate[i].ticketGateStatus = buttonState;
+    }
+    if ((millis() - delayButtonExit[i]) > DEBOUNCE_INTERVAL) {
+      buttonState = digitalRead(tGate[i].PIN_EXIT);/*se o botão estiver pressionado, retorna HIGH*/
+      if (buttonState) {
+        delayButtonExit[i] = millis();
+        if (buttonState != tGate[i].exitStatus) { //Se o botão de EXIT for pressionado, executar esse bloco
+//          Serial.print(F("botão EXIT-"));
+//          Serial.print(i);
+//          Serial.println(F(" pressionado"));
+          sendPayload("botão EXIT-" + String(i) + " pressionado");
+        }
+        tGate[i].exitStatus = true;
+      }
+      tGate[i].exitStatus = buttonState;
+    }
+    //    digitalWrite(tGate[i].PIN_LED_GREEN, !digitalRead(tGate[i].PIN_LED_GREEN));
+    //    digitalWrite(tGate[i].PIN_LED_RED, !digitalRead(tGate[i].PIN_LED_GREEN));
+  }
+}
+
+void pinsSetup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(tGate[i].PIN_BTN_CHECK, INPUT);
+    pinMode(tGate[i].PIN_BTN_TICKET_GATE, INPUT);
+    pinMode(tGate[i].PIN_EXIT, INPUT);
+    pinMode(tGate[i].PIN_LED_GREEN, OUTPUT);
+    pinMode(tGate[i].PIN_LED_RED, OUTPUT);
+  }
+}
+
+void wifiSetup() {
   Serial.print("Conectando ao ");
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);//Configura para "station mode" assim não cria uma rede AP, interferindo na comunicação
@@ -22,60 +120,47 @@ void wifiSetup(){
     delay(500);
     Serial.print(".");
   }
-  Serial.println("WiFi conectado");
-  Serial.println("endereço IP: ");
+  Serial.println(F("WiFi conectado"));
+  Serial.println(F("endereço IP: "));
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  Serial.begin(115200);
-  wifiSetup();//Estabelece conexão com o AP
-}
-
-void loop() {
-  Serial.print("<< se conectando com ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("<< falha na conexão");
+bool sendPayload(String message) {
+  String serverResponse = "";
+  if (message == "")
+    return false;
+  // A classe WiFiClient é utilizada para criar conexões TCP
+  WiFiClient _client;
+  if (!_client.connect(host, port)) {
+    Serial.println(F("<< falha na conexão"));
     delay(5000);
-    return;
+    return false;
   }
-
-  // This will send a string to the server
-  Serial.println("<< enviando dados para o servidor");
-  if (client.connected()) {
-    client.print("<< olá de ESP32");
-    client.print("<< estou online");
+  //A mensagem é enviada para o servidor caso haja conexão
+  if (_client.connected()) {
+    _client.print(message);//Somente 1 mensagem por conexão
   }
-
-  // wait for data to be available
+  //aguarda os dados ficarem disponíveis
   unsigned long timeout = millis();
-  while (client.available() == 0) {
+  while (_client.available() == 0) {
     if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(6000);
-      return;
+      Serial.println(F(">>> Tempo de conexão excedido!"));
+      _client.stop();
+      delay(3000);
+      return false;
     }
   }
-
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("<< recebendo do servidor remoto");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
+  //Lê todas as linhas da resposta do servidor
+//  Serial.println(F("<< Recebendo do servidor remoto: "));
+  while (_client.available()) {
+    char character = static_cast<char>(_client.read());
+    serverResponse += character; 
   }
-
+  Serial.println(serverResponse);
   // Close the connection
-  Serial.println("____");
-  Serial.println("<< encerrando conexão");
-  client.stop();
-
-  delay(6000); // execute once every 5 minutes, don't flood remote service
+//  Serial.println("\n<< encerrando conexão");
+  _client.stop();
+  if(serverResponse != "")
+    return true;
+  return false;
 }
--- Servidor_WEMOSD1_USB1.ino
+++ Servidor_WEMOSD1_USB1.ino
@@ -1,71 +1,93 @@
 /*
- * Servidor
- * WEMOS D1
- * /dev/ttyUSB1
- * COM10
- */
- 
+   Servidor
+   WEMOS D1
+   /dev/ttyUSB1
+   COM10
+*/
+
 #include <ESP8266WiFi.h>
-#include <WiFiServer.h>
+#include <WiFiServer.h> 
+#include <SSD1306Wire.h>
+
+#define DISPLAY_TITLE "Server v0.0.0.1"
 
 const char* ssid     = "NodeMCU_AP";
 const char* password = "pa$$word";
- 
-WiFiServer sv(555);//Cria o objeto servidor na porta 555
-WiFiClient cl;//Cria o objeto cliente.
- 
-void tcp();
 
-void setup()
-{
-    Serial.begin(115200);//Habilita a comm serial.
- 
-    WiFi.mode(WIFI_AP);//Define o WiFi como Acess_Point.
-    WiFi.softAP(ssid, password);//Cria a rede de Acess_Point.
- 
-    sv.begin();//Inicia o servidor TCP na porta declarada no começo.
+SSD1306Wire  display(0x3c, D2, D1);
+WiFiServer _server(555);//Cria o objeto servidor na porta 555
+WiFiClient _client;//Cria o objeto cliente.
+
+void tcp();
+void wifiSetup();
+void writeLineDisplay(int pixelBeginX, int line, String text);
+void setupDisplay();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nIniciando...");
+  wifiSetup();
+  setupDisplay();
 }
- 
-void loop()
-{
-    tcp();//Funçao que gerencia os pacotes e clientes TCP.
+
+uint32_t verificador = 0;
+
+void loop() {
+  tcp();//Funçao que gerencia os pacotes e clientes TCP.
 }
- 
-void tcp()
-{
-    if (cl.connected())//Detecta se há clientes conectados no servidor.
-    {
-        if (cl.available() > 0)//Verifica se o cliente conectado tem dados para serem lidos.
-        {
-            String req = "";
-            while (cl.available() > 0)//Armazena cada Byte (letra/char) na String para formar a mensagem recebida.
-            {
-                char z = cl.read();
-                req += z;
-            }
- 
-            //Mostra a mensagem recebida do cliente no Serial Monitor.
-            Serial.print("\nUm cliente enviou uma mensagem");
-            Serial.print("\n...IP do cliente: ");
-            Serial.print(cl.remoteIP());
-            Serial.print("\n...IP do servidor: ");
-            Serial.print(WiFi.softAPIP());
-            Serial.print("\n...Mensagem do cliente: " + req + "");
- 
-            //Envia uma resposta para o cliente
-            cl.print("\n>>--------------------------------");
-            cl.print("\n>>O servidor recebeu sua mensagem");
-            cl.print("\n>>...Seu IP: ");
-            cl.print(cl.remoteIP());
-            cl.print("\n>>...IP do Servidor: ");
-            cl.print(WiFi.softAPIP());
-            cl.print("\n>>...Sua mensagem: " + req + "");
-            cl.print("\n>>++++++++++++++++++++++++++++++++");
-        }
+
+void wifiSetup() {
+  WiFi.mode(WIFI_AP);//Define o WiFi como Acess_Point.
+  WiFi.softAP(ssid, password);//Cria a rede de Acess_Point.
+  _server.begin();//Inicia o servidor TCP na porta declarada no começo.
+}
+
+void tcp() {
+  //Detecta se há clientes conectados no servidor.
+  if (_client.connected()) {
+    //Verifica se o cliente conectado tem dados para serem lidos.
+    if (_client.available() > 0) {
+      String msg_received = "";
+      //Armazena cada Byte (letra/char) na String para formar a mensagem recebida.
+      while (_client.available() > 0) {
+        char character = _client.read();
+        msg_received += character;
+      }
+      //Mostra a mensagem recebida do cliente no Serial Monitor.
+      Serial.print("\n...Mensagem do cliente: " + msg_received + "");
+      //Envia uma resposta para o cliente
+      _client.println(">> O servidor recebeu sua mensagem: " + msg_received);
+      writeLineDisplay(0, 1, msg_received);
+      
     }
-    else//Se nao houver cliente conectado,
-    {
-        cl = sv.available();//Disponabiliza o servidor para o cliente se conectar.
-        delay(1);
-    }
+  }
+  //Se nao houver cliente conectado,
+  else {
+    _client = _server.available();//Disponabiliza o servidor para o cliente se conectar.
+    delay(1);
+  }
+}
+
+void writeLineDisplay(int pixelBeginX, int line, String text){
+  display.setColor(BLACK);
+  uint8_t pixelY = (line * 10) + (6-line);
+  display.fillRect(pixelBeginX, pixelY+3, 128, 9);//coluna, linha, largura, altura
+  display.setColor(WHITE);
+  display.drawString(pixelBeginX, pixelY, text);
+  display.display();
+}
+
+void setupDisplay(){
+  display.init();
+  display.flipScreenVertically();
+  //Apaga o display
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  //Seleciona a fonte
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, DISPLAY_TITLE);
+  display.setFont(ArialMT_Plain_10);
+  for(uint8_t i = 1; i<6; i++)
+    writeLineDisplay(0, i, String(i));
+  display.display();
 }
